4c0c73edf8d87f08c27048543f0ea3c9aa475de5
pasadi <parna80as@gmail.com>
2022-11-02 18:05:41 +0330

    add lectures and page

--- src/components/pages/Home/HomePage.js
+++ src/components/pages/Home/HomePage.js
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import Dice from "../../assets/image/dice.png";
 import Aboutus from "../Aboutus/Aboutus";
+import Logo from "../../assets/image/logo.png";
 
 const HomePage = () => {
   return (
@@ -35,6 +36,34 @@ const HomePage = () => {
         </div>
         <div className="md:w-5/12 sm:w-screen flex justify-center items-center">
           <img src={Dice} alt="" width={"80%"} />
+        </div>
+      </section>
+      <section className="mt-20 mx-auto max-w-screen-xl pb-4 px-4 sm:px-8 mb-3 flex flex-col md:flex-row">
+        <div className="md:w-5/12 sm:w-screen flex justify-center items-center">
+          <img src={Logo} alt="" width={"80%"} />
+        </div>
+        <div className="md:w-7/12 sm:w-screen	">
+          <div className="text-center space-y-4 mb-3 md:w-50 sm:w-100">
+            <h1 className="text-indigo-600 font-bold text-xl md:text-4xl">
+              تدریس مجازی آمار و احتمال مهندسی
+            </h1>
+            <p
+              className="text-gray-600 max-w-xl mx-auto leading-relaxed text-justify "
+              style={{ whiteSpace: "pre-line" }}
+            >
+              محتوای آموزشی رایگان و آنلاین برای درس «آمار و احتمال مهندسی»،جهت
+              آماده سازی محتوای این سایت از مباحث تدریس شده دکتر بهرک، استاد
+              محترم دانشکده برق و کامپیوتر دانشگاه تهران استفاده کردیم!
+            </p>
+          </div>
+          <div className="mt-12 justify-center items-center space-y-3 sm:space-x-6 sm:space-y-0 sm:flex">
+            <a
+              href="https://openbookshelf.github.io/ProbStat/#/"
+              className=" md:m-2 py-3 px-6 md:w-40  sm:w-screen bg-pink-600 text-white text-center rounded-md shadow-md block  hover:text-white hover:bg-pink-800"
+            >
+                سایت آمار و احتمال مهندسی{" "}
+            </a>
+          </div>
         </div>
       </section>
       <section className="my-16">
